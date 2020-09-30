b726c9241a97118cbdc54a3f7eab04ad3b96b729
Ilya Belskiy <ilya.belskiy.1998@mail.ru>
2020-09-30 14:40:19 +0300

    init: add function in index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!array) {
+    return 0;
+ }
+
+  if (array.length == 0) {
+    return 0;
+  }
+
+  let max = 9999;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < max) {
+      max = array[i];
+    } 
+  }
+  return max;
+};
+  
+
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (!array) {
+    return 0;
+ }
+  
+  if (array.length == 0) {
+    return 0;
+  }
+
+  let min = -9999;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > min) {
+      min = array[i]
+    }
+  }
+  return min;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (!array) {
+    return 0;
+ }
+  
+  if (array.length == 0) {
+    return 0;
+  }
+  
+  const total = array.reduce((a, b) => a + b, 0);
+  return total / array.length;
+  
+};
